f6048afbd2de4b248331a9a873e51b6d62200fb7
adavies312 <a.m.davies1020@gmail.com>
2018-04-20 19:16:26 -0500

    Update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,79 +1,86 @@
 $(document).ready(function () {
+
 //variables
-score = 0;
-wins = 0;
-losses = 0;
+var score = 0;
+var wins = 0;
+var losses = 0;
+
 //display variables in html
-$("totalScore").html(score);
+$("#totalScore").html(score);
 $("#wins").html(wins);
 $("#losses").html(losses);
 
-//random number to guess
-var randomNum = 18 + Math.floor(Math.random() * 102);
+//random number
+var randomNum = 18 + Math.floor(Math.random() * 120);
 console.log(randomNum);
 $("#randomNumber").html(randomNum);
 
-//assign random numbers to each crystal
-purpleCrystal = 1 + Math.floor(Math.random() * 12);
-redCrystal = 1 + Math.floor(Math.random() * 12);
-greenCrystal = 1 + Math.floor(Math.random() * 12);
-blueCrystal = 1 + Math.floor(Math.random() * 12);
 
-var reset = function () {
+//assign random numbers for each crystal
+
+var purpleCrystal = 1 + Math.floor(Math.random() * 12);
+var redCrystal = 1 + Math.floor(Math.random() * 12);
+var greenCrystal = 1 + Math.floor(Math.random() * 12);
+var blueCrystal = 1 + Math.floor(Math.random() * 12);
+
+//reset function
+var reset = function() {
     score = 0;
     //reset random number
-    var randomNum = 18 + Math.floor(Math.random() * 102);
+    randomNum = 18 + Math.floor(Math.random() * 120);
     console.log(randomNum);
     //reset crystal values
-    purpleCrystal = 1 + Math.floor(Math.random() * 12);
-    redCrystal = 1 + Math.floor(Math.random() * 12);
-    greenCrystal = 1 + Math.floor(Math.random() * 12);
     blueCrystal = 1 + Math.floor(Math.random() * 12);
+    pinkCrystal = 1 + Math.floor(Math.random() * 12);
+    greenCrystal = 1 + Math.floor(Math.random() * 12);
+    purpleCrystal = 1 + Math.floor(Math.random() * 12);
+    $("#randomNumber").html(randomNum);
     $("#totalScore").html(score);
     $("#wins").html(wins);
     $("#losses").html(losses);
-    $("#randomNumber").html(randomNum);
 }
-//conditions for winning or losing
-var results = function () {
-    if (score === randomNum) {
+
+//conditions for winning / losing
+var results = function() {
+    if (score == randomNum) {
         wins++;
         $("#wins").html(wins);
-        reset ();
+        reset();
+
     } else if (score > randomNum) {
         losses++;
         $("#losses").html(losses);
-        reset ();
+        reset();
     }
-}
-//clicking on crystals adds to score
-//each click runs results function to determine if won or lost
+};
 
-$("#purple").on("click", function () {
+//click the crystal to add to score
+//run results function to see if win, loss, or keep playing
+$("#purple").on("click", function() {
     score = score + purpleCrystal;
     console.log(score);
     $("#totalScore").html(score);
-    results ();
-});
+    results();
+})
 
 $("#red").on("click", function() {
-    score = score + redCrystal;
+    score = score + pinkCrystal;
     console.log(score);
     $("#totalScore").html(score);
     results();
-});
+})
 
 $("#green").on("click", function() {
     score = score + greenCrystal;
     console.log(score);
     $("#totalScore").html(score);
     results();
-});
+})
 
 $("#blue").on("click", function() {
     score = score + blueCrystal;
     console.log(score);
     $("#totalScore").html(score);
     results();
-});
+})
 });
