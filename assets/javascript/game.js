1dd9462a7e261b59b98b24a63e98224a225ae24b
adavies312 <a.m.davies1020@gmail.com>
2018-04-22 03:35:30 -0500

    Update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,33 +1,33 @@
 $(document).ready(function () {
 
-    //variables
+    // basic variables
     var score = 0;
     var wins = 0;
     var losses = 0;
 
-    //update variables in html
+    // update variables in html
     $("#totalScore").html(score);
     $("#wins").html(wins);
     $("#losses").html(losses);
 
-    //pick a random number
+    // pick a random number
     var randomNum = 18 + Math.floor(Math.random() * 102);
     console.log(randomNum);
     $("#randomNumber").html(randomNum);
 
-    //assign random numbers for each crystal
+    // assign random numbers to each crystal
     var purpleCrystal = 1 + Math.floor(Math.random() * 12);
     var redCrystal = 1 + Math.floor(Math.random() * 12);
     var greenCrystal = 1 + Math.floor(Math.random() * 12);
     var blueCrystal = 1 + Math.floor(Math.random() * 12);
 
-    //create a reset function
+    // create a reset function
     var reset = function () {
         score = 0;
-        //reset random number
+        //assign new random number
         randomNum = 18 + Math.floor(Math.random() * 102);
         console.log(randomNum);
-        //reset crystal values
+        //assign new crystal values
         purpleCrystal = 1 + Math.floor(Math.random() * 12);
         redCrystal = 1 + Math.floor(Math.random() * 12);
         greenCrystal = 1 + Math.floor(Math.random() * 12);
@@ -38,7 +38,7 @@ $(document).ready(function () {
         $("#losses").html(losses);
     }
 
-    //conditions for winning / losing
+    // conditions for winning / losing stored in results function 
     var results = function () {
         if (score == randomNum) {
             wins++;
@@ -52,7 +52,8 @@ $(document).ready(function () {
         }
     };
 
-    //click the crystal to add to score
+    // click the crystal to add to score
+    // check to see if user won each click -> calls results function 
     $("#purple").on("click", function () {
         score = score + purpleCrystal;
         console.log(score);
