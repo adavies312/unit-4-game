c5878322c43249f30acace2dea1060160fc00a66
adavies312 <a.m.davies1020@gmail.com>
2018-04-22 05:30:00 -0500

    Update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,11 @@
 $(document).ready(function () {
 
-    // basic variables
+    // initialize variables
     var score = 0;
     var wins = 0;
     var losses = 0;
 
-    // update variables in html
+    // update the dom
     $("#totalScore").html(score);
     $("#wins").html(wins);
     $("#losses").html(losses);
@@ -13,6 +13,7 @@ $(document).ready(function () {
     // pick a random number
     var randomNum = 18 + Math.floor(Math.random() * 102);
     console.log(randomNum);
+    // update the DOM
     $("#randomNumber").html(randomNum);
 
     // assign random numbers to each crystal
@@ -21,8 +22,65 @@ $(document).ready(function () {
     var greenCrystal = 1 + Math.floor(Math.random() * 12);
     var blueCrystal = 1 + Math.floor(Math.random() * 12);
 
-    // create a restart function
-    var restart = function () {
+    // click the crystal to add to score
+    // check conditions to see if user won each click 
+    // calls results function 
+    
+    $("#purple").on("click", function () {
+        score = score + purpleCrystal;
+        console.log(score);
+        $("#totalScore").html(score);
+        // Check conditions for win or loss
+        results();
+    })
+
+    $("#red").on("click", function () {
+        score = score + redCrystal;
+        console.log(score);
+        $("#totalScore").html(score);
+        // Check conditions for win or loss
+        results();
+    })
+
+    $("#green").on("click", function () {
+        score = score + greenCrystal;
+        console.log(score);
+        $("#totalScore").html(score);
+        // Check conditions for win or loss
+        results();
+    })
+
+    $("#blue").on("click", function () {
+        score = score + blueCrystal;
+        console.log(score);
+        $("#totalScore").html(score);
+        // Check conditions for win or loss
+        results();
+    })
+
+    // conditions for winning / losing stored in results function 
+    var results = function () {
+        if (score === randomNum) {
+            wins++;
+            //update DOM
+            $("#wins").html(wins);
+            //reset the game
+            reset();
+
+        } else if (score > randomNum) {
+            losses++;
+            //update DOM
+            $("#losses").html(losses);
+            //reset the game
+            reset();
+        }
+    };
+
+    // create a reset function
+    // Resets score to 0
+    // Updates random number and values of crystal 
+    // Update the DOM
+    var reset = function () {
         score = 0;
         //assign new random number
         randomNum = 18 + Math.floor(Math.random() * 102);
@@ -37,48 +95,4 @@ $(document).ready(function () {
         $("#wins").html(wins);
         $("#losses").html(losses);
     }
-
-    // conditions for winning / losing stored in results function 
-    var results = function () {
-        if (score == randomNum) {
-            wins++;
-            $("#wins").html(wins);
-            restart();
-
-        } else if (score > randomNum) {
-            losses++;
-            $("#losses").html(losses);
-            restart();
-        }
-    };
-
-    // click the crystal to add to score
-    // check to see if user won each click -> calls results function 
-    $("#purple").on("click", function () {
-        score = score + purpleCrystal;
-        console.log(score);
-        $("#totalScore").html(score);
-        results();
-    })
-
-    $("#red").on("click", function () {
-        score = score + redCrystal;
-        console.log(score);
-        $("#totalScore").html(score);
-        results();
-    })
-
-    $("#green").on("click", function () {
-        score = score + greenCrystal;
-        console.log(score);
-        $("#totalScore").html(score);
-        results();
-    })
-
-    $("#blue").on("click", function () {
-        score = score + blueCrystal;
-        console.log(score);
-        $("#totalScore").html(score);
-        results();
-    })
 })
